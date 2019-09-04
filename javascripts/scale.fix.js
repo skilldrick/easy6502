437b1794b2fb3d97f0b3b535ec99182d2a70f400
William Trindade <williamtrindade777@gmail.com>
2019-09-04 14:25:22 -0300

    Update var to let
    Let is more recomended for use in local scope variables

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,5 +1,5 @@
-var metas = document.getElementsByTagName('meta');
-var i;
+let metas = document.getElementsByTagName('meta');
+let i;
 if (navigator.userAgent.match(/iPhone/i)) {
   for (i=0; i<metas.length; i++) {
     if (metas[i].name == "viewport") {
